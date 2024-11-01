dde9bc9c1941b52e1aa4b068552109b137fb83da
baris can <bariscanaslan@outlook.com>
2024-11-01 16:40:48 +0300

    final

--- Group06.java
+++ Group06.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
  
-public class CourseProject1 {
+public class Group06 {
 
     /* Statistical Information About an Array Starts Here */
 
